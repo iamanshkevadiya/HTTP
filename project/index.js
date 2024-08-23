5134c519f647ce1e98eed2e0a054e17206c4d703
Ansh Kevadiya <146837647+iamanshkevadiya@users.noreply.github.com>
2024-08-23 09:17:09 +0530

    Update index.js

--- project/index.js
+++ project/index.js
@@ -10,21 +10,21 @@ const server = http.createServer((req, res) => {
                 res.end("Error creating index.html file");
             }
             else {
-                res.end("<h1>Wlcome to My Node-JS Website</h1> <p>You Can write you code here.</p> <span>Back-end Devalopment</span>");
+                res.end(`<img id="img" /> <h1 id="title">Wlcome to My Node-JS Website</h1> <p id="grid">You Can write you code here.</p> <span id="course">Back-end Devalopment</span>`);
             }
         });
     }
     else if(req.url === "/login"){
-        res.end("<h1>Login Page</h1> <p>You have an account please Login First</p> <span>Back-end Devalopment</span>");
+        res.end(`<h1>Login Page</h1> <p>You have an account please Login First</p> <span>Back-end Devalopment</span>`);
     }
     else if(req.url === "/signup"){
-        res.end("<h1>Signup Page</h1> <p>Please Signup For More Details..</p> <span>Back-end Devalopment</span>");
+        res.end(`<h1>Signup Page</h1> <p>Please Signup For More Details..</p> <span>Back-end Devalopment</span>`);
     }
     else if(req.url === "/contact"){
-        res.end("<h1>Contact Us</h1> <p>Contact us on xyz142@gmail.com</p> <span>Back-end Devalopment</span>");
+        res.end(`<h1>Contact Us</h1> <p>Contact us on xyz142@gmail.com</p> <span>Back-end Devalopment</span>`);
     }
     else if(req.url === "/service"){
-        res.end("<h1>Service Page</h1> <p>Our Services to help you for your Success.</p> <span>Back-end Devalopment</span>");
+        res.end(`<h1>Service Page</h1> <p>Our Services to help you for your Success.</p> <span>Back-end Devalopment</span>`);
     }
     else{
         res.end("<h1>Error</h1>");
